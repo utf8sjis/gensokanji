b7acfcf7dcf89af70c387929c4e7f5d5e8693f1c
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2021-10-03 17:31:05 +0900

    fix: Fix issue where viewport height on mobile devices is not as intended

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+// 画面高さいっぱいの値を取得しvhをCSS変数にセットする
+const setViewportHeight = function() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+setViewportHeight();
+
+// リサイズ時にもvhを再取得（ただし画面幅が変わったときのみ）
+let vw = window.innerWidth;
+window.addEventListener('resize', () => {
+  if (vw !== window.innerWidth) {
+    vw = window.innerWidth;
+    setViewportHeight();
+  }
+});
+
+
+// 読み込み後明示的にアニメーションを開始する
 window.onload = function() {
-  // 明示的にアニメーションを開始する
   setTimeout(() => {
     const actElements = document.querySelectorAll(
       '.header__logo-container,' +
