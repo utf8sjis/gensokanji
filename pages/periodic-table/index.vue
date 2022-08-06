ccb0934b4523c41695f55527618eb4b46b646ed6
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-08-06 22:23:11 +0900

    fix: Fix head method

--- pages/periodic-table/index.vue
+++ pages/periodic-table/index.vue
@@ -54,72 +54,74 @@ export default {
     }
   },
 
-  head: {
-    title,
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。中国語圏では、化学元素の命名は漢字1字でなされ、部首で気体か金属かなどの性質を表します。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
+  head() {
+    return {
+      title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。中国語圏では、化学元素の命名は漢字1字でなされ、部首で気体か金属かなどの性質を表します。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
+        },
+        // OGP
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: (title ? `${title} - ` : '') + process.env.npm_package_name,
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'article',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: baseURL + '/periodic-table',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: (title ? `${title} - ` : '') + process.env.npm_package_name,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: baseURL + '/img/periodic_table.png',
+        },
+      ],
+      link: [
+        // micron.js
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://unpkg.com/webkul-micron@1.1.6/dist/css/micron.min.css',
+        },
+      ],
+      script: [
+        // micron.js
+        {
+          type: 'text/javascript',
+          src: 'https://unpkg.com/webkul-micron@1.1.6/dist/script/micron.min.js',
+        },
+        // Twitter wedgets.js
+        // https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+        {
+          hid: 'twitterSnippet',
+          innerHTML: twitterSnippet,
+        },
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        twitterSnippet: ['innerHTML'],
       },
-      // OGP
-      {
-        hid: 'og:site_name',
-        property: 'og:site_name',
-        content: (title ? `${title} - ` : '') + process.env.npm_package_name,
-      },
-      {
-        hid: 'og:type',
-        property: 'og:type',
-        content: 'article',
-      },
-      {
-        hid: 'og:url',
-        property: 'og:url',
-        content: baseURL + '/periodic-table',
-      },
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: (title ? `${title} - ` : '') + process.env.npm_package_name,
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content:
-          '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: baseURL + '/img/periodic_table.png',
-      },
-    ],
-    link: [
-      // micron.js
-      {
-        rel: 'stylesheet',
-        type: 'text/css',
-        href: 'https://unpkg.com/webkul-micron@1.1.6/dist/css/micron.min.css',
-      },
-    ],
-    script: [
-      // micron.js
-      {
-        type: 'text/javascript',
-        src: 'https://unpkg.com/webkul-micron@1.1.6/dist/script/micron.min.js',
-      },
-      // Twitter wedgets.js
-      // https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
-      {
-        hid: 'twitterSnippet',
-        innerHTML: twitterSnippet,
-      },
-    ],
-    __dangerouslyDisableSanitizersByTagID: {
-      twitterSnippet: ['innerHTML'],
-    },
+    }
   },
 
   mounted() {
