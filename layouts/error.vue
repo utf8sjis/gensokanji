1dd1b5823f1e1780a9d13cc8a2b5378fdc0fdb4b
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-08-07 14:45:10 +0900

    update: Change design of error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,11 +2,10 @@
   <div class="page">
     <div class="content-wrapper">
       <div class="content">
-        <h1>{{ content.headline }}</h1>
-        <p>{{ content.message }}</p>
-        <p>
-          <a href="/" class="u-link u-link--gk">トップへ戻る</a>
-        </p>
+        <h1>{{ error.statusCode }}</h1>
+        <h2>{{ error.message }}</h2>
+        <p>{{ description }}</p>
+        <p><a href="/" class="u-link u-link--gk">トップへ戻る</a></p>
       </div>
     </div>
     <gensokanji-footer />
@@ -21,57 +20,21 @@ export default {
     error: { type: Object, default: null },
   },
 
-  data() {
-    return {
-      contentDict: {
-        400: {
-          headline: '400 Bad Request',
-        },
-        401: {
-          headline: '401 Unauthorized',
-        },
-        403: {
-          headline: '403 Forbidden',
-        },
-        404: {
-          headline: '404 Not Found',
-          message: 'お探しのページは見つかりませんでした',
-        },
-        408: {
-          headline: '408 Request Timeout',
-        },
-        500: {
-          headline: '500 Internal Server Error',
-        },
-        502: {
-          headline: '502 Bad Gateway',
-        },
-        503: {
-          headline: '503 Service Unavailable',
-        },
-        504: {
-          headline: '504 Gateway Timeout',
-        },
-      },
-    }
-  },
-
   head() {
     return {
-      title: this.content.headline,
+      title: this.description,
     }
   },
 
   computed: {
-    content() {
-      return {
-        headline:
-          this.contentDict[this.error.statusCode]?.headline ??
-          this.error.statusCode + ' Error',
-        message:
-          this.contentDict[this.error.statusCode]?.message ??
-          'エラーが発生しました',
+    description() {
+      const contentDict = {
+        404: { description: 'お探しのページは見つかりませんでした' },
       }
+      return (
+        contentDict[this.error.statusCode]?.description ??
+        'エラーが発生しました'
+      )
     },
   },
 }
@@ -104,13 +67,15 @@ export default {
   gap: 10px;
   text-align: center;
   transform: translateY(-15px);
+}
 
-  h1 {
-    @include g.font(en2);
-    font-size: 48px;
-    @include g.breakpointMax {
-      font-size: 36px;
-    }
-  }
+h1 {
+  @include g.font(en2);
+  font-size: 48px;
+}
+
+h2 {
+  @include g.font(en2);
+  font-size: 24px;
 }
 </style>
