761fddae6fe6cced723674ed7b7ac2b4e1933a47
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-10-13 22:33:42 +0900

    update: Change header illustration

--- components/gensokanji/layout/GensokanjiHeader.vue
+++ components/gensokanji/layout/GensokanjiHeader.vue
@@ -19,10 +19,20 @@
       />
     </div>
     <div
+      class="island"
+      :style="{ transform: `translate(0, ${scrollY * 0.4}px)` }"
+    >
+      <img
+        src="@/assets/img/gensokanji/island.svg"
+        alt="安定の島"
+        class="island__image"
+      />
+    </div>
+    <div
       class="heading-container"
       :style="{
         transform: `translate(0, ${scrollY * 0.5}px)`,
-        opacity: 1 - scrollY * 0.005,
+        opacity: 1 - scrollY * 0.004,
       }"
     >
       <h1 class="heading">{{ heading }}</h1>
@@ -41,8 +51,14 @@
         </div>
       </div>
     </div>
-    <div class="skyline">
-      <div class="skyline__image"></div>
+    <div
+      class="wave2"
+      :style="{ transform: `translate(0, ${scrollY * 0.2}px)` }"
+    >
+      <div class="wave2__image"></div>
+    </div>
+    <div class="wave1">
+      <div class="wave1__image"></div>
     </div>
   </header>
 </template>
@@ -89,12 +105,12 @@ export default {
 header {
   position: relative;
   overflow: hidden;
-  padding: 70px 15px 60px;
+  padding: 90px 15px 80px;
   @include g.breakpoint(g.$tablet) {
-    padding: 70px 10% 60px;
+    padding: 90px 10% 80px;
   }
   @include g.breakpoint() {
-    padding: 70px 15% 60px;
+    padding: 90px 15% 80px;
   }
 }
 
@@ -153,19 +169,48 @@ header {
   }
 }
 
-.skyline {
+.island {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0 40px;
+  border-bottom: 30px solid transparent;
+  @include g.breakpoint(g.$tablet) {
+    padding: 0 70px;
+  }
+
+  &__image {
+    height: 65px;
+  }
+}
+
+.wave2 {
+  $colorWave2: #363e54;
   position: absolute;
   bottom: 0;
   left: 0;
   width: 100%;
-  min-width: 150px * 5; // 必ずheightいっぱいにして画像が1つは入る大きさ
-  height: 150px;
-  transform: translateY(100px);
+  min-width: 20px * 15; // height - border = 画像の高さ を常に維持するのに十分なwidth
+  height: 20px + 30px;
+  border-bottom: 30px solid $colorWave2;
 
   &__image {
     height: 100%;
-    background: url('@/assets/img/gensokanji/skyline1.svg') repeat-x 0% 0% /
-      contain;
+    background: url('@/assets/img/gensokanji/wave2.svg') repeat-x -10px 0 / contain;
+  }
+}
+
+.wave1 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  min-width: 25px * 15; // height = 画像の高さ を常に維持するのに十分なwidth
+  height: 25px;
+
+  &__image {
+    height: 100%;
+    background: url('@/assets/img/gensokanji/wave1.svg') repeat-x 0 0 / contain;
   }
 }
 </style>
